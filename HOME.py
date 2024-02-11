813a53c75605c66d584fc50b95f09acab76f175b
Tavish Gupta <97957388+Tavishgupta@users.noreply.github.com>
2024-02-11 23:53:59 +0530

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -5,7 +5,7 @@ import json
 def home():
     st.header("FACE RECOGNITION ATTENDANCE SYSTEM\n")
     st.text("\n\n")
-    path = "/Users/tavishgupta/Desktop/AP_Project/Animations/Animation - 1697706571258.json"
+    path = "Animations/Animation - 1697706571258.json"
     with open(path,"r") as file: 
         url = json.load(file) 
     st_lottie(url, 
